804df32b0a38e3d24c40ab87fa27f036c3b502cd
Geovana Souza <geovanasouza0712@gmail.com>
2025-06-16 22:19:25 -0300

    Tipos de dados

---
+++ Python-I/input.py
@@ -0,0 +1,14 @@
+name = input("Digite o nome do jogo\n")
+yearLaunch = int(input("Digite o ano de lançamento\n"))
+gamePrice = float(input("Digite o valor do jogo\n"))
+noteRating = float(input("Digite a nota de avaliação do jogo\n"))
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(noteRating)
+
+print (type(name))
+print (type(yearLaunch))
+print (type(gamePrice))
+print (type(noteRating))
---
+++ Python-I/tipo-de-dados.py
@@ -0,0 +1,15 @@
+name = "The Sims 4"
+yearLaunch = 2014
+gamePrice = 0.00
+planIncluded = False
+
+print (name)
+print (yearLaunch)
+print (gamePrice)
+print (planIncluded)
+
+print (type(name))
+print (type(yearLaunch))
+print (type(gamePrice))
+print (type(planIncluded))
+
