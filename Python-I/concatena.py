56a67590417915a991912c23837006e90ccb0534
Geovana Souza <geovanasouza0712@gmail.com>
2025-06-17 22:03:05 -0300

    Concatenação

---
+++ Python-I/concatena.py
@@ -0,0 +1,12 @@
+name = input("Digite o nome do jogo\n")
+yearLaunch = int(input("Digite o ano de lançamento\n"))
+gamePrice = float(input("Digite o valor do jogo\n"))
+
+# print("Nome do Jogo:", name)
+# print("Ano do Jogo:", yearLaunch)
+# print("Preço do Jogo:", gamePrice)
+
+
+# print("Nome do Jogo:", name, "\n Ano de lançamento:", yearLaunch, "\n Preço do Jogo:", gamePrice)
+
+print(f"Nome do Jogo: {name} \n Ano de lançamento: {yearLaunch} \n Preço do jogo: {gamePrice}")
