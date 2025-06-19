eeb2e6a746fb1451bec1cbf6a775bf6346f11b31
Geovana Souza <geovanasouza0712@gmail.com>
2025-06-18 22:31:34 -0300

    revisando sobre operadores

---
+++ Python-I/excercicios/ex02.py
@@ -0,0 +1,23 @@
+w# jeito errado
+# num1  = int(input("Digite um número: "))
+# num1 -=1
+# print(f"O antecessor desse número é: {num1}")
+# num1 +=1
+# print(f"O sucessor desse número é: {num1}")
+
+
+num1 = int(input("Digite um número: "))
+
+antecessor = num1
+antecessor -= 1
+
+sucessor = num1
+sucessor += 1
+
+print(f"O antecessor desse número é: {antecessor}")
+print(f"O sucessor desse número é: {sucessor}")
+
+#Resolução 1: Antecessor e Sucessor Número
+# number = int(input("Digite um número\\n"))
+
+# print(f"Antecessor do número: {number -1} | Sucessor do número: {number +1}")
