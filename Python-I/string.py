3c53d0cf20cab58db952b6ca2f3f7bfd9e68ee10
Geovana Souza <geovanasouza0712@gmail.com>
2025-06-19 21:40:04 -0300

    trabalhando com strings

---
+++ Python-I/string.py
@@ -0,0 +1,11 @@
+gameName = "Palia"
+
+gameDescription = """
+Palia é um jogo de simulação de vida
+desenvolvido pela Singularity 6,
+com foco em atividades como pesca, culinária e agricultura.
+Permite jogar sozinho ou com amigos online.
+"""
+
+print(gameName)
+print(gameDescription)
