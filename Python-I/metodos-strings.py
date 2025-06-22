8356162b7ffb761d2f737ace0d04736517c4a8a4
Geovana Souza <geovanasouza0712@gmail.com>
2025-06-21 21:33:30 -0300

    Métodos de strings em python

---
+++ Python-I/metodos-strings.py
@@ -0,0 +1,14 @@
+name = "Sherlock Code"
+description = """
+Aula para desvendar os mistérios da programação, com lógica afiada e raciocínio preciso.
+"""
+
+print(name.upper()) #Retornar strings tudo Maiúsculo
+print(name.lower()) # Minúsculo
+print(name.capitalize()) #Apenas primeira letra maiúscula
+print(name.title()) #Apenas primeira letra maiúscula
+print(name.center(19, '-')) #Retorna a string centralizada, esse valor 19 já conta com o valor da string, então tem que colcoar um valor que ambos os lados fique igual ou não precisa que os lagos fique igual
+print(name.find("e")) #Retorna a posição de um determinado caractere
+print(description.count('a')) #Conta quantos caracteres, iguais a ele, se o ã for asim, ele não conta
+print(description.replace("Aula", "Assunto")) #Altera valor de uma string
+print(description.split(','))
