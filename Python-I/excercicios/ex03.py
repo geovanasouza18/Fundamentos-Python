d5f7cecc403993b9e03434690442be529a5c3a9d
Geovana Souza <geovanasouza0712@gmail.com>
2025-06-21 21:14:20 -0300

    Praticando split e slice

---
+++ Python-I/excercicios/ex03.py
@@ -0,0 +1,15 @@
+nameText = "Relatório final Confidencial"
+words = nameText.split()
+print(words)
+print (nameText[10:15])
+
+name = "Geovana Holmes"
+print(name[:7])
+
+name2 = "Python"
+print(name2[-3:])
+
+print(name2[::-1])
+
+namePares = "desenvolvedora"
+print(namePares[::2])
