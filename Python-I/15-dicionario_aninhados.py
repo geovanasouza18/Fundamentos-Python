61cdeb5c6a602eb66c3d61cecd41e5a1759083ae
Geovana Souza <geovanasouza0712@gmail.com>
2026-01-12 18:12:59 -0300

    Atualizações

--- Python-I/15-dicionario_aninhados.py
+++ Python-I/15-dicionario_aninhados.py
@@ -1,3 +1,21 @@
 import pprint
 
-gameDict = 
+gameDict = {
+    'Palia':{
+        'ano': 2023,
+        'clasificacao': 9.0,
+        'genero': ['Massively Multiplayer Online', 'Aventura e Exploração']
+    },
+    'Mario odyssey':{
+        'ano': 2017,
+        'clasificacao': 10.0,
+        'genero': ['3D', 'Aventura']
+    },
+    'The Sims 4':{
+        'ano': 2014,
+        'clasificacao': 8.5,
+        'genero': 'Simulador de vida'
+    }
+}
+pp = pprint.PrettyPrinter(depth=5)
+pp.pprint(gameDict)
